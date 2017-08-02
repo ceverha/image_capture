b4f046069280cdbcc04542b6c1e69f72bb59ff0b
ceverha <jack.ceverha@gmail.com>
2017-08-02 21:49:32 +0000

    works on raspicam

--- camera.py
+++ camera.py
@@ -6,8 +6,19 @@ import sys
 import base64
 import json
 import datetime
+import picamera
+import numpy as np
 
 deviceId = 'raspberry_pi'
+# initiate distance sensor
+dist.distanceInit()
+THRESHOLD = 30
+# refractory period size in seconds
+PERIOD_SIZE = 5
+
+camera = picamera.PiCamera()
+camera.resolution = (1024, 768)
+
 def upload(filenames):
     endpoint = "https://eai5udmepa.execute-api.us-east-1.amazonaws.com/latest/match"
     payload = '{"numImages": ' + str(len(filenames)) + ','
@@ -25,41 +36,35 @@ def upload(filenames):
     headers = {'Content-Type': 'application/json'}
     return requests.post(endpoint, headers=headers, json=payload)
 
-def saveResizedImages(filename, srcImage):
-    image1 = srcImage
-    image2 = cv2.resize(frame, None, fx=0.75, fy=1.0, interpolation = cv2.INTER_NEAREST)
-    image3 = cv2.resize(frame, None, fx=1.0, fy=0.75, interpolation = cv2.INTER_NEAREST)
+def saveResizedImages(filename, image):
+    image1 = image
+    image2 = cv2.resize(image, None, fx=0.8, fy=1.0, interpolation=cv2.INTER_NEAREST)
+    image3 = cv2.resize(image, None, fx=1.0, fy=0.8, interpolation=cv2.INTER_NEAREST)
     cv2.imwrite(filename+'_1.jpg', image1)
     cv2.imwrite(filename+'_2.jpg', image2)
     cv2.imwrite(filename+'_3.jpg', image3)
     filenames = [filename+'_1.jpg', filename+'_2.jpg', filename+'_3.jpg']
     return filenames
     
-vc = cv2.VideoCapture(0)
-
-# initiate distance sensor
-dist.distanceInit()
-THRESHOLD = 30
-
-# refractory period size in seconds
-PERIOD_SIZE = 5
-
 lastPeriodTime = time.time()
 try:
     while True:
         # don't overload rpio
         time.sleep(0.25)
-        print(str(datetime.datetime.now()))
         distance = dist.getDistance()
         if (distance < THRESHOLD):
             currTime = time.time()
             if currTime > lastPeriodTime + PERIOD_SIZE:
                 lastPeriodTime = currTime
 
-                # capture and save image
-                rval, frame = vc.read()
-                filename = "webcam_capture"
-                filenames = saveResizedImages(filename, frame)
+                width = camera.resolution[0]
+                height = camera.resolution[1]
+                image = np.empty((height*width*3,), dtype=np.uint8) 
+                camera.capture(image, 'bgr')
+                image = image.reshape((height, width, 3))
+
+                filename = 'raspicam_capture'
+                filenames = saveResizedImages(filename, image)
 
                 before = time.time()
                 response = upload(filenames)
--- raspi_test.py
+++ raspi_test.py
@@ -1,6 +1,31 @@
 import picamera
 import time
+import numpy as np
+import cv2
 
+face_cascade = cv2.CascadeClassifier("./haarcascade_frontalface_default.xml")
 camera = picamera.PiCamera()
-camera.start_preview()
-time.sleep(30)
+
+# camera.resolution = (1024, 768)
+width = camera.resolution[0]
+height = camera.resolution[1] + 8
+
+print('warming up')
+time.sleep(2)
+
+image = np.empty((height*width*3,), dtype=np.uint8) 
+camera.capture(image, 'bgr')
+image = image.reshape((height, width, 3))
+
+gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+before = time.time()
+faces = face_cascade.detectMultiScale(gray, scaleFactor=1.15, minNeighbors=5)
+after = time.time()
+print("detect finished in " + str(after - before) + " seconds")
+print(faces)
+
+buf = 20
+for (x,y,w,h) in faces:
+    cv2.rectangle(image, (x-buf, y-buf), (x+w+buf, y+h+buf), (0,255,0), 2)
+
+cv2.imwrite('iot.jpg', image)
